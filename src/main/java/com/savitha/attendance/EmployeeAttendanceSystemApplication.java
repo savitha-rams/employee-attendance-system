77653ef31222f62e7fbb6bb9a90b5ca57db10f19
Savitha <Savitha@Savitha-PC>
2026-06-20 21:56:54 +0800

    Initial Spring Boot project setup

---
+++ src/main/java/com/savitha/attendance/EmployeeAttendanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.savitha.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAttendanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAttendanceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savitha/attendance/EmployeeAttendanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savitha.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAttendanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
