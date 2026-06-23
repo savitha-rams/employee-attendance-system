781034dcafa07f092b42c7ae95731a9f5a9c8607
Savitha Ramasamy <savitha.ramasamy@gmail.com>
2026-06-23 19:49:48 +0800

    baseline Employee module

---
+++ src/main/java/com/savitha/attendance/entity/Employee.java
@@ -0,0 +1,25 @@
+package com.savitha.attendance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String department;
+}
