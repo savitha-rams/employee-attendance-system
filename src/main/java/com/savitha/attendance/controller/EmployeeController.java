8bbc701c020f3e0b4e527f21c530d9d2eea4252b
Savitha Ramasamy <savitha.ramasamy@gmail.com>
2026-06-25 20:29:04 +0800

    Updated the Employee Controller for CRUD operations

--- src/main/java/com/savitha/attendance/controller/EmployeeController.java
+++ src/main/java/com/savitha/attendance/controller/EmployeeController.java
@@ -1,16 +1,54 @@
 package com.savitha.attendance.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.savitha.attendance.entity.Employee;
+import com.savitha.attendance.repository.EmployeeRepository;
+
 
 @RestController
-@RequestMapping("/employees")
 public class EmployeeController {
 
-    @GetMapping
-    public String getEmployees() {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+//	@GetMapping
+  /**  public String getEmployees() {
         return "Employee API Working";
+    }*/
+    
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+    
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeRepository.findById(id).orElse(new Employee());
+    }
+    
+    @PostMapping("/employees")
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+    
+    @PutMapping("/employees/{id}")
+    public Employee updateEmployeeById(@PathVariable Long id, @RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+    
+    @DeleteMapping("/employees/{id}")
+    public void deleteProductById(@PathVariable Long id) {
+    	employeeRepository.deleteById(id);
     }
 }
--- src/main/java/com/savitha/attendance/entity/Employee.java
+++ src/main/java/com/savitha/attendance/entity/Employee.java
@@ -1,17 +1,9 @@
 package com.savitha.attendance.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "employees")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Employee {
 
     @Id
@@ -22,4 +14,35 @@ public class Employee {
     private String lastName;
     private String email;
     private String department;
+    
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
 }
